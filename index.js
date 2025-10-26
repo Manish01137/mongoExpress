71a01b6cd799997a08552d5d63d940fbe53bca45
Manish01137 <manishBTECH23@ced.alliance.edu.in>
2025-10-26 10:55:45 +0530

    done

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const Chat = require("./models/chat.js")
 app.set("views",path.join(__dirname,"views"));
 app.set("views engine", "ejs");
 app.use(express.static(path.join(__dirname,"public")));
-
+app.use(express.urlencoded({extended: true}));
 
 main()
  .then(() => {
@@ -20,13 +20,62 @@ async function main(){
 }
 
 
-
+// index Route
 app.get("/chats",async(req,res) => {
     let chats =  await Chat.find();
     console.log(chats);
     res.render("index.ejs",{chats});
 })
 
+
+//New Route
+app.get("/chats/new", (req,res) => {
+    res.render("new.ejs");
+})
+
+// create Route 
+
+app.post("/chats",(req,res) => {
+    let {from, to , msg} = req.body;
+    let newChat = new Chat({
+        from:from,
+        to: to,
+        msg: msg,
+        created_at: new Date()
+    });
+    // console.log(newChat);
+
+
+    newChat
+    .save()
+    .then(res => {
+        console.log("chat was saved")
+    })
+    .catch(err => {console.log(err)});
+    res.redirect("/chats");
+});
+
+//edit route
+
+app.get("/chats/:id/edit", async(req ,res) => {
+    let {id} = req.params;
+    let chat = await Chat.findById(id);
+    res.render("edit.ejs", {chat});
+})
+
+
+// update route
+
+app.put("/chats/:id", async(req,res)=>{
+    let {id} = req.params;
+    let {newMsg} = req.body;
+    let updatedChat = await Chat.findByIdAndUpdate(id, 
+        {msg:newMsg},
+        {runValidators:true,new:true}
+    );
+    console.log(updatedChat);
+    res.redirect("/chats");
+})
 
 app.get("/",(req,res) => {
     res.send("root is working")
